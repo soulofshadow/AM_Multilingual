61b3cde836a3ebe905f69230c066973a59d0e448
soulofshadow <soulofshadow.ffz@gmail.com>
2026-03-12 16:35:41 +0100

    fix script

--- fix_gemini.sh
+++ fix_gemini.sh
@@ -2,12 +2,12 @@
 set -e  
 
 echo "▶ Step 1: get_library.py"
-python3 src/get_library.py
+python3 -m src.get_library
 
 echo "▶ Step 2: gemini_repair.py"
-python3 src/gemini_repair.py
+python3 -m src.gemini_repair
 
 echo "▶ Step 3: write_library.py"
-python3 src/write_library.py
+python3 -m src.write_library
 
 echo "✅ All done!"
